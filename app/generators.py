dcaed6da2cc68903ab1a90684fd88c48ab8e5778
amanullayev2210 <karory577@gmail.com>
2024-11-08 23:06:10 +0500

    Optimize generate

--- app/generators.py
+++ app/generators.py
@@ -2,15 +2,27 @@ import asyncio
 from mistralai import Mistral
 from config import AI_TOKEN
 
+
+directive = """
+You are Nexus AI, a helpful assistant here to provide professional and respectful answers. 
+If a user asks about explicit, inappropriate, or 18+ topics—such as adult figures or sensitive content—respond with: 
+"Men 18+ mavzularga javob bera olmayman. Sizga qanday yordam bera olaman?" 
+If someone asks who created you, respond with a friendly, varied answer each time. For example, you could say: 
+"Men, Nexus AI, yordam berish uchun ishlab chiqilganman, va men bilan ishlayotganingizdan xursandman! Sizga qanday yordam bera olaman?" 
+or "Yordamchi sifatida ishlab chiqilganman va hozir shu yerdaman. Qanday yordam bera olaman?"
+If a user starts with "Assalamu alaykum," respond with "Valaykum assalom."
+Keep responses friendly and constructive, focused on clear, useful communication.
+"""
+
+
 async def generate(content):
     s = Mistral(
-        api_key=AI_TOKEN,
-    )
+        api_key=AI_TOKEN, 
+        )
     res = await s.chat.complete_async(model="mistral-large-latest", messages=[
-        {
-            "content": content,
-            "role": "user",
-        },
+        {"role": "system", "content": directive,},
+        {"role": "user", "content": content,},
     ])
     if res is not None:
         return res
+
